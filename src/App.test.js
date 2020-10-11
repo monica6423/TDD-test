a21fc542d2acacda00274352442471c350773d0c
monica6423 <monica6423@gmail.com>
2020-10-11 11:31:51 +0800

    setup jest test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//create a suite by using describe 
+//pass into a string and a function
+//string is a label for what is the suite covering 
+describe('App', () => {
+  it('renders without crashing', ()=> {
+    //shallow is a way of forcing it not  to load the
+    //entire child tree of the app component
+    //we are only loading app
+    const appWrapper = shallow(<App />)
+  });
 });
